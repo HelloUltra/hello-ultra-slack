e16a353c62a3d3889bca39d934f7506edfa1a7e0
권용근 <kingbbode@gmail.com>
2017-01-22 22:05:30 +0900

    first commit

---
+++ src/main/java/com/ultra/HelloUltraSlackApplication.java
@@ -0,0 +1,12 @@
+package com.ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUltraSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUltraSlackApplication.class, args);
+	}
+}
